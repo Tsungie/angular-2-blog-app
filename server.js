458552dae263cd17f8027cf94779ab3da83999ad
tncube <tncube@zimhisp.org>
2018-08-03 17:15:44 +0200

    did up to linking with the service

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 //create an http server
 const http = require('http');
 const app = express();
+const api = require('./server/routes/api');
 
 //setup our data for handling icoming requests
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({extended:false}))//it will tell the parser not to
 
 //we going to tell express to save static files from angulars dist folder
 app.use(express.static(path.join(__dirname,'dist')));
-
+app.use('/api',api);
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'dist/index.html'));
 })
