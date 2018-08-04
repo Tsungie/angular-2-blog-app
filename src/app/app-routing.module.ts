65cb3b1dd8599a6fdea7d0b861f0c148e53b5115
tncube <tncube@zimhisp.org>
2018-08-04 12:07:26 +0200

    create article...but the button going nowhere

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
 
@@ -14,7 +15,11 @@ const routes: Routes = [
   {
     path: 'articles/:id',
     component: ArticleComponent
-  }
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
 ];
 
 @NgModule({
